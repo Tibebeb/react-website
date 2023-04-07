36ae331e350c7b44cdc92dc22c7fda955cc8f8db
Tibebe <tibebesolomon633@gmail.com>
2023-04-07 17:15:29 +0300

    navbar

---
+++ src/commponets/HeroSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeroSection(){
+    <div className='hero-container'>
+        <video src='/videos/video-2.mp4' autoPlay loopmuted>
+            
+        </video>
+    </div>
+}
